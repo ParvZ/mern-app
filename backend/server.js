fe58d8e61b788fe262afcee18c562a33526e6605
ParvZ <pemran@rocketmail.com>
2022-06-10 19:36:43 +0600

    added form and getting data from backend on client

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+var cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(cors());
 
 app.get('/', (req,res)=>{
     res.send('Hi, from mern app')
